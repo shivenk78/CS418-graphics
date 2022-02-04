e685f85228a6bf338fb02e45aa7b86b970ac128c
shivenk78 <kumar.shiven15@gmail.com>
2022-02-03 18:14:06 -0600

    Completed Mosaic (MP1 done)

--- MP1/mp1.js
+++ MP1/mp1.js
@@ -51,6 +51,15 @@ var vertRangeForWiden = 0.2;
 /** @global Factor by which to widen the Block I */
 var horizWidenFactor = 1.2;
 
+/** @global Second VAO for the "MacroLeaf" mosaic */
+var mosaicVertexArrayObject;
+/** @gloabl Decides whether to show mosaic or Dancing I */
+var isMosaicEnabled = false;
+/** @global Current "starting color" for the mosaic triangles*/
+var mosaicColor = [1.0, 0.0, 0.0, 1.0];
+/** @global Keeps track of when to iterate mosaic color */
+var mosaicIteration = 0;
+
 /**
  * Translates degrees to radians
  * @param {Number} degrees Degree input to function
@@ -156,14 +165,23 @@ function setupShaders() {
  * Set up the buffers to hold the triangle's vertex positions and colors.
  */
 function setupBuffers() {
+  // Store radio button status
+  isMosaicEnabled = document.getElementById("MacroLeaf").checked == true;
+
   // Create the vertex array object, which holds the list of attributes for
   // the triangle.
   vertexArrayObject = gl.createVertexArray();
-  gl.bindVertexArray(vertexArrayObject);
+  mosaicVertexArrayObject = gl.createVertexArray();
+  isMosaicEnabled
+    ? gl.bindVertexArray(mosaicVertexArrayObject)
+    : gl.bindVertexArray(vertexArrayObject);
 
   // Create a buffer for positions, and bind it to the vertex array object.
   vertexPositionBuffer = gl.createBuffer();
-  gl.bindBuffer(gl.ARRAY_BUFFER, vertexPositionBuffer);
+  mosaicPositionBuffer = gl.createBuffer();
+  isMosaicEnabled
+    ? gl.bindBuffer(gl.ARRAY_BUFFER, mosaicPositionBuffer)
+    : gl.bindBuffer(gl.ARRAY_BUFFER, vertexPositionBuffer);
 
   // Define a triangle in clip coordinates.
   // prettier-ignore
@@ -173,75 +191,154 @@ function setupBuffers() {
   //   0.5, -0.5, 0.0,
   // ];
 
-  // Define BLOCK I in clip coordinates.
-  // prettier-ignore
-  var vertices = [
-    0.5, 0.75, 0.0, -0.5, 0.75, 0.0, -0.5, 0.5, 0.0,
-    0.5, 0.75, 0.0, 0.5, 0.5, 0.0, -0.5, 0.5, 0.0,
-    0.2, 0.5, 0.0, -0.2, 0.5, 0.0, -0.2, -0.5, 0.0,
-    0.2, 0.5, 0.0, 0.2, -0.5, 0.0, -0.2, -0.5, 0.0,
-    0.5, -0.75, 0.0, -0.5, -0.75, 0.0, -0.5, -0.5, 0.0,
-    0.5, -0.75, 0.0, 0.5, -0.5, 0.0, -0.5, -0.5, 0.0,
-  ];
+  if (!isMosaicEnabled) {
+    // Define BLOCK I in clip coordinates.
+    // prettier-ignore
+    var vertices = [
+      0.5, 0.75, 0.0, -0.5, 0.75, 0.0, -0.5, 0.5, 0.0,
+      0.5, 0.75, 0.0, 0.5, 0.5, 0.0, -0.5, 0.5, 0.0,
+      0.2, 0.5, 0.0, -0.2, 0.5, 0.0, -0.2, -0.5, 0.0,
+      0.2, 0.5, 0.0, 0.2, -0.5, 0.0, -0.2, -0.5, 0.0,
+      0.5, -0.75, 0.0, -0.5, -0.75, 0.0, -0.5, -0.5, 0.0,
+      0.5, -0.75, 0.0, 0.5, -0.5, 0.0, -0.5, -0.5, 0.0,
+    ];
 
-  var vertexSize = 3;
-  var numVertices = vertices.length / vertexSize;
+    var vertexSize = 3;
+    var numVertices = vertices.length / vertexSize;
 
-  // CUSTOM DIRECT ANIMATION - top-to-bottom "widening" of the Block I
+    // CUSTOM DIRECT ANIMATION - top-to-bottom "widening" of the Block I
 
-  vertPosToWiden -= 0.005;
-  if (vertPosToWiden < -1) vertPosToWiden = 1;
-  for (var i = 0; i < numVertices; i++) {
-    var yPos = vertices[i * vertexSize + 1];
-    console.log("hi", yPos); //Math.abs(yPos - vertPosToWiden));
-    if (Math.abs(yPos - vertPosToWiden) < vertRangeForWiden) {
-      // modify x-coordinate to "widen" it
-      vertices[i * 3] *= horizWidenFactor;
-      console.log(vertices);
+    vertPosToWiden -= 0.005;
+    if (vertPosToWiden < -1) vertPosToWiden = 1;
+    for (var i = 0; i < numVertices; i++) {
+      var yPos = vertices[i * vertexSize + 1];
+      if (Math.abs(yPos - vertPosToWiden) < vertRangeForWiden) {
+        // modify x-coordinate to "widen" it
+        vertices[i * 3] *= horizWidenFactor;
+      }
     }
+
+    // Populate the buffer with the position data.
+    gl.bufferData(gl.ARRAY_BUFFER, new Float32Array(vertices), gl.DYNAMIC_DRAW);
+    vertexPositionBuffer.itemSize = vertexSize;
+    vertexPositionBuffer.numberOfItems = numVertices;
+
+    // Binds the buffer that we just made to the vertex position attribute.
+    gl.vertexAttribPointer(
+      shaderProgram.vertexPositionAttribute,
+      vertexPositionBuffer.itemSize,
+      gl.FLOAT,
+      false,
+      0,
+      0
+    );
+
+    // Do the same steps for the color buffer.
+    vertexColorBuffer = gl.createBuffer();
+    gl.bindBuffer(gl.ARRAY_BUFFER, vertexColorBuffer);
+    // prettier-ignore
+    var ORANGE = [0.91, 0.29, 0.15, 1.0];
+    var colors = [];
+    // generate same orange color for every vertex
+    for (var i = 0; i < vertexPositionBuffer.numberOfItems; i++) {
+      for (var j = 0; j < ORANGE.length; j++) colors.push(ORANGE[j]);
+    }
+
+    gl.bufferData(gl.ARRAY_BUFFER, new Float32Array(colors), gl.DYNAMIC_DRAW);
+    vertexColorBuffer.itemSize = 4;
+    vertexColorBuffer.numItems = colors.length / vertexColorBuffer.itemSize;
+    gl.vertexAttribPointer(
+      shaderProgram.vertexColorAttribute,
+      vertexColorBuffer.itemSize,
+      gl.FLOAT,
+      false,
+      0,
+      0
+    );
+
+    // Enable each attribute we are using in the VAO.
+    gl.enableVertexAttribArray(shaderProgram.vertexPositionAttribute);
+    gl.enableVertexAttribArray(shaderProgram.vertexColorAttribute);
+  } else {
+    var vertices = [];
+
+    // programatically generate the triangles for the MacroLeaf mosaic
+    const numSquares = 8;
+    const squareSize = (2/numSquares);
+    var x = -1;
+    var y = 1;
+    for (var r = 0; r < numSquares; r++) {
+      x = -1;
+      for (var c = 0; c < numSquares; c++) {
+        vertices.push(x, y, 0.0);
+        vertices.push(x+squareSize, y, 0.0);
+        vertices.push(x, y-squareSize, 0.0);
+        vertices.push(x+squareSize, y, 0.0);
+        vertices.push(x, y-squareSize, 0.0);
+        vertices.push(x+squareSize, y-squareSize, 0.0);
+        x += squareSize;
+      }
+      y-=squareSize;
+    }
+
+    // Populate the buffer with the position data.
+    gl.bufferData(gl.ARRAY_BUFFER, new Float32Array(vertices), gl.DYNAMIC_DRAW);
+    mosaicPositionBuffer.itemSize = 3;
+    mosaicPositionBuffer.numberOfItems = vertices.length/mosaicPositionBuffer.itemSize;
+
+    const numTriangles = mosaicPositionBuffer.numberOfItems / 3;
+
+    // Binds the buffer that we just made to the vertex position attribute.
+    gl.vertexAttribPointer(
+      shaderProgram.vertexPositionAttribute,
+      mosaicPositionBuffer.itemSize,
+      gl.FLOAT,
+      false,
+      0,
+      0
+    );
+
+    // Do the same steps for the color buffer.
+    vertexColorBuffer = gl.createBuffer();
+    gl.bindBuffer(gl.ARRAY_BUFFER, vertexColorBuffer);
+        
+    var colors = [];
+    // duplicate mosaicColor as a temp
+    var color = mosaicColor.map((x) => x);
+    const colorIncrem = 0.01;
+    // generate color for each triangle
+    for (var i = 0; i < numTriangles; i++) {
+      for (var j = 0; j < 3; j++) {
+        color.map((x) => colors.push(x));
+      }
+
+      // increment color
+      color = incrementColor(color, colorIncrem);
+    }
+    // increment mosaic starting color for next iteration
+    if (mosaicIteration > 5) {
+      mosaicIteration = 0;
+      mosaicColor = incrementColor(mosaicColor, colorIncrem);
+    } else {
+      mosaicIteration++;
+    }
+
+    gl.bufferData(gl.ARRAY_BUFFER, new Float32Array(colors), gl.DYNAMIC_DRAW);
+    vertexColorBuffer.itemSize = 4;
+    vertexColorBuffer.numItems = colors.length / vertexColorBuffer.itemSize;
+    gl.vertexAttribPointer(
+      shaderProgram.vertexColorAttribute,
+      vertexColorBuffer.itemSize,
+      gl.FLOAT,
+      false,
+      0,
+      0
+    );
+
+    // Enable each attribute we are using in the VAO.
+    gl.enableVertexAttribArray(shaderProgram.vertexPositionAttribute);
+    gl.enableVertexAttribArray(shaderProgram.vertexColorAttribute);
   }
-
-  // Populate the buffer with the position data.
-  gl.bufferData(gl.ARRAY_BUFFER, new Float32Array(vertices), gl.DYNAMIC_DRAW);
-  vertexPositionBuffer.itemSize = vertexSize;
-  vertexPositionBuffer.numberOfItems = numVertices;
-
-  // Binds the buffer that we just made to the vertex position attribute.
-  gl.vertexAttribPointer(
-    shaderProgram.vertexPositionAttribute,
-    vertexPositionBuffer.itemSize,
-    gl.FLOAT,
-    false,
-    0,
-    0
-  );
-
-  // Do the same steps for the color buffer.
-  vertexColorBuffer = gl.createBuffer();
-  gl.bindBuffer(gl.ARRAY_BUFFER, vertexColorBuffer);
-  // prettier-ignore
-  var ORANGE = [0.91, 0.29, 0.15, 1.0];
-  var colors = [];
-  // generate same orange color for every vertex
-  for (var i = 0; i < vertexPositionBuffer.numberOfItems; i++) {
-    for (var j = 0; j < ORANGE.length; j++) colors.push(ORANGE[j]);
-  }
-
-  gl.bufferData(gl.ARRAY_BUFFER, new Float32Array(colors), gl.DYNAMIC_DRAW);
-  vertexColorBuffer.itemSize = 4;
-  vertexColorBuffer.numItems = colors.length / vertexColorBuffer.itemSize;
-  gl.vertexAttribPointer(
-    shaderProgram.vertexColorAttribute,
-    vertexColorBuffer.itemSize,
-    gl.FLOAT,
-    false,
-    0,
-    0
-  );
-
-  // Enable each attribute we are using in the VAO.
-  gl.enableVertexAttribArray(shaderProgram.vertexPositionAttribute);
-  gl.enableVertexAttribArray(shaderProgram.vertexColorAttribute);
 
   // Unbind the vertex array object to be safe.
   gl.bindVertexArray(null);
@@ -258,7 +355,9 @@ function draw() {
   gl.clear(gl.COLOR_BUFFER_BIT);
 
   // Use the vertex array object that we set up.
-  gl.bindVertexArray(vertexArrayObject);
+  isMosaicEnabled
+    ? gl.bindVertexArray(mosaicVertexArrayObject)
+    : gl.bindVertexArray(vertexArrayObject);
 
   // Send the ModelView matrix with our transformations to the vertex shader.
   gl.uniformMatrix4fv(
@@ -268,7 +367,9 @@ function draw() {
   );
 
   // Render the triangle.
-  gl.drawArrays(gl.TRIANGLES, 0, vertexPositionBuffer.numberOfItems);
+  isMosaicEnabled
+    ? gl.drawArrays(gl.TRIANGLES, 0, mosaicPositionBuffer.numberOfItems)
+    : gl.drawArrays(gl.TRIANGLES, 0, vertexPositionBuffer.numberOfItems);
   // Unbind the vertex array object to be safe.
   gl.bindVertexArray(null);
 }
@@ -278,7 +379,6 @@ function draw() {
  * each frame.
  */
 function animate(currentTime) {
-  // Read the speed slider from the web page.
   var speed = 5;
 
   // Convert the time to seconds.
@@ -290,24 +390,26 @@ function animate(currentTime) {
 
   // Update geometry to rotate 'speed' degrees per second.
   rotAngle += speed * deltaTime;
-  if (rotAngle > 360.0) rotAngle = 0.0;
+  if (rotAngle > 360.0 || isMosaicEnabled) rotAngle = 0.0;
   glMatrix.mat4.fromZRotation(modelViewMatrix, degToRad(rotAngle));
 
-  // Update geometry to scale the Block I
-  if (currBlockScale <= MIN_BLOCK_SCALE) {
-    currBlockScale = MIN_BLOCK_SCALE;
-    isBlockScaleIncreasing = true;
-  } else if (currBlockScale >= MAX_BLOCK_SCALE) {
-    currBlockScale = MAX_BLOCK_SCALE;
-    isBlockScaleIncreasing = false;
+  if (!isMosaicEnabled) {
+    // Update geometry to scale the Block I
+    if (currBlockScale <= MIN_BLOCK_SCALE) {
+      currBlockScale = MIN_BLOCK_SCALE;
+      isBlockScaleIncreasing = true;
+    } else if (currBlockScale >= MAX_BLOCK_SCALE) {
+      currBlockScale = MAX_BLOCK_SCALE;
+      isBlockScaleIncreasing = false;
+    }
+
+    currBlockScale += isBlockScaleIncreasing
+      ? 0.01 * speed * deltaTime
+      : -0.01 * speed * deltaTime;
+
+    var scaleVec = [currBlockScale, currBlockScale, currBlockScale];
+    glMatrix.mat4.scale(modelViewMatrix, modelViewMatrix, scaleVec);
   }
-
-  currBlockScale += isBlockScaleIncreasing
-    ? 0.01 * speed * deltaTime
-    : -0.01 * speed * deltaTime;
-
-  var scaleVec = [currBlockScale, currBlockScale, currBlockScale];
-  glMatrix.mat4.scale(modelViewMatrix, modelViewMatrix, scaleVec);
   setupBuffers();
 
   // Draw the frame.
@@ -329,4 +431,37 @@ function startup() {
   setupBuffers();
   gl.clearColor(0.0, 0.0, 0.0, 1.0);
   requestAnimationFrame(animate);
+}
+
+/**
+ *
+ * @param {Array} color Array of 4 floats, representing an RGBA color to be incremented
+ * @param {float} increment Amount to increment the color by
+ * @returns {Array} Incremented color
+ */
+function incrementColor(color, increment) {
+  if (color[0] >= 1.0 && color[1] == 0 && color[2] == 0) {
+    color[0] = 1 - increment;
+    color[1] = increment;
+  } else if (color[0] == 0.0 && color[1] >= 1.0 && color[2] == 0) {
+    color[1] = 1 - increment;
+    color[2] = increment;
+  } else if (color[0] == 0.0 && color[1] == 0.0 && color[2] >= 1.0) {
+    color[2] = 1 - increment;
+    color[0] = increment;
+  } else if (color[0] > 0 && color[1] > 0) {
+    color[0] = Math.max(0, color[0] - increment);
+    color[1] = Math.min(1, color[1] + increment);
+  } else if (color[1] > 0 && color[2] > 0) {
+    color[1] = Math.max(0, color[1] - increment);
+    color[2] = Math.min(1, color[2] + increment);
+  } else if (color[0] > 0 && color[2] > 0) {
+    color[2] = Math.max(0, color[2] - increment);
+    color[0] = Math.min(1, color[0] + increment);
+  }
+  for (var i = 0; i < color.length - 1; i++) {
+    if (color[i] < increment) color[i] = 0;
+    if (color[i] > 1 - increment) color[i] = 1;
+  }
+  return color;
 }
